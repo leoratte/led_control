5397e805550408141dc2de346743ad113c26dd6a
Leonard Anderweit <leonard.anderweit@stud.tu-darmstadt.de>
2020-03-10 00:46:03 +0100

    now works with new web-app

--- install.sh
+++ install.sh
@@ -4,9 +4,6 @@ pip3 install websockets
 rm pigpio.py
 systemctl enable pigpiod
 systemctl start pigpiod
-IP=$(hostname -I)
-IP=${IP%% *}
-sed -i "s/localhost/$IP/g" html/script.js
 cp html/* /var/www/html
 service nginx start
 nohup python3 start.py &
