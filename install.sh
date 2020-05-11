232608ea5854b5208a26990587216e93432d5461
Leonard Anderweit <leonard.anderweit@stud.tu-darmstadt.de>
2020-05-11 18:53:24 +0200

    changed to use systemd service

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 apt install pigpio nginx python3
 pip3 install websockets
-rm pigpio.py
+mkdir /opt/led_control
+cp -t=/opt/led_control start.py control.py led.py animation.py util.py config.json
+cp led_control.service /etc/systemd/system
 systemctl enable pigpiod
 systemctl start pigpiod
+systemctl enable led_control.service
+systemctl start led_control.service
 cp html/* /var/www/html
 service nginx start
-nohup python3 start.py &
