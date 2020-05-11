7ac3d67bca5e3cdb4c30dd6def4fd638dad63155
Leonard Anderweit <leonard.anderweit@stud.tu-darmstadt.de>
2020-05-11 19:11:16 +0200

    bugfix

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 apt install pigpio nginx python3 python3-pip -y
-pip3 install websockets, pigpio
+pip3 install websockets
+pip3 install pigpio
 mkdir /opt/led_control
-cp -t=/opt/led_control led.py start.py animation.py control.py util.py config.json
+cp -t /opt/led_control led.py start.py animation.py control.py util.py config.json
 cp led_control.service /etc/systemd/system
 systemctl enable pigpiod
 systemctl start pigpiod
